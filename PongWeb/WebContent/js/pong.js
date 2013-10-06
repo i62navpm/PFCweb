c743033bba76f74c738b4c83d66f18ac35b1d71d
Manolo <Manolo@manolo-q9exyzdf>
2013-10-06 14:03:14 +0200

    Tablero y primera raqueta

--- PongWeb/WebContent/js/pong.js
+++ PongWeb/WebContent/js/pong.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
+	/*
+	 * Se ajusta el tamaño del div que contiene el canvas
+	 * para que la altura sea la mitad de la altura
+	*/
+	$("#container").css("height",parseFloat($("#container").css("width"))/2);
+	var W = parseFloat($("#container").css("width"));
+	var H = parseFloat($("#container").css("height"));
+	
 	function initStage(){
-		/*
-		 * Se ajusta el tamaño del div que contiene el canvas
-		 * para que la altura sea la mitad de la altura
-		*/
-		$("#container").css("height",parseFloat($("#container").css("width"))/2);
-		var W = parseFloat($("#container").css("width"));
-		var H = parseFloat($("#container").css("height"));
-		
 		/*
 		 * Creamos nuestro escenario con las dimensiones del contenedor div
 		 */
@@ -35,8 +35,8 @@ $(document).ready(function(){
 		    var linePart = new Kinetic.Rect({
 		        x : parseInt(W/2),
 		        y : y,
-		        width : 4,
-		        height : 20,
+		        width : W/140,
+		        height : H/15,
 		        fill : 'white'
 		    });
 		    middleLine.add(linePart);
@@ -47,10 +47,75 @@ $(document).ready(function(){
 		 */
 		backgroundLayer.add(background);
 		backgroundLayer.add(middleLine);
+		
+		// foreground
+        var foregroundLayer = new Kinetic.Layer();
+        
+        // player
+        var player = new Player();
+        foregroundLayer.add(player);
+        
 		stage.add(backgroundLayer);
+		stage.add(foregroundLayer);
+		
+		document.onkeydown = function(event) {
+			 event = event || window.event;
 
+			 var e = event.keyCode;
+
+			 if (e == 83 || e == 40) {
+				 player.moveDown();
+				 
+             } else if (e == 87 || e == 38 ) {
+                 player.moveUp();
+			};
+			player.draw();
+			
+			foregroundLayer.draw();
+			stage.draw();
+			
+		};
 	};
 	
+	function Player(){
+		var config = {
+                fill: 'white',
+                x : W/10,
+                y : H/2-(H/5)/2,
+                width: W/50,
+                height: H/5,
+                draggable: true,
+                dragBoundFunc: function(pos) {
+                  return {
+                    x: this.getAbsolutePosition().x,
+                    y: pos.y
+                  };
+                }
+            };
+            Kinetic.Rect.call(this, config);
+            this.speed = 10;
+            this.name = 'Player';
+	};
+	Player.prototype = new Kinetic.Rect({});
+    Player.prototype.constructor = Player;
+    
+    /*
+     * Función para mover abajo la raqueta
+     */
+    Player.prototype.moveDown = function() {
+    	if (this.attrs.y < H-(this.attrs.height/2)) 
+            this.setY(this.getY()+this.speed);
+        
+    };
+    
+    /*
+     * Función para mover arriba la raqueta
+     */
+    Player.prototype.moveUp = function(playerSpeed) {
+        if (this.attrs.y > -(this.attrs.height/2))
+        	this.setY(this.getY()-this.speed);
+    };
+    
 	$(function(){
 	    initStage();
 	});
