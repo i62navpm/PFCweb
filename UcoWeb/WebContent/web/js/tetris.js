c00ba556fde5ab8a7f83d8e6ebe1f587665cc7ae
Manolo <Manolo@manolo-q9exyzdf>
2014-01-18 15:40:52 +0100

    Menú general para pong y tetris mejorado

--- UcoWeb/WebContent/web/js/tetris.js
+++ UcoWeb/WebContent/web/js/tetris.js
@@ -24,11 +24,7 @@ $(document).ready(function(){
 	            clearTimeout(id);
 	        };
 	}());
-	
-	
-	
-	var stage = null;
-	
+
 	function initStage(){
 		stage = new Kinetic.Stage({
 		    container : "container",
@@ -38,8 +34,8 @@ $(document).ready(function(){
 		
 		var background = new Background();
 		var foreground = new Foreground();
-
-		new Game(background, foreground);
+		var menu = new initMenu(stage);
+		new Game(background, foreground,menu);
 
 //		stage.on('dbltap dblclick', function() {
 //    		if (!running)
@@ -153,29 +149,31 @@ $(document).ready(function(){
 		this.nSquares = 4;
 		this.nTypes = 7;
 		this.skyline = this.pForeground.rows-1;
+		this.mode = '1color';
 		
+		this.eventsGame();
 		//Piece _|_
-		this.xPiece[1]= new Array(0, 1,-1, 0);
-		this.yPiece[1]= new Array(0, 0, 0, 1);
+		this.xPiece[1]= new Array(0, 1, 0, -1);
+		this.yPiece[1]= new Array(0, 0, 1, 0);
 		
 		//Piece |__
 		this.xPiece[2]= new Array(0, 1,-1,-1);
-		this.yPiece[2]= new Array(0, 0, 0, 1);
+		this.yPiece[2]= new Array(0, 0, 1, 0);
 		
 		//Piece __|
-		this.xPiece[3]= new Array(0, 1,-1, 1);
-		this.yPiece[3]= new Array(0, 0, 0, 1);
+		this.xPiece[3]= new Array(0, 1, 1,-1);
+		this.yPiece[3]= new Array(0, 0, 1, 0);
 		
 		//Piece _|-
-		this.xPiece[4]= new Array(0,-1, 1, 0);
+		this.xPiece[4]= new Array(0,-1, 0, 1);
 		this.yPiece[4]= new Array(0, 0, 1, 1);
 		
 		//Piece -|_
-		this.xPiece[5]= new Array(0, 1,-1, 0);
+		this.xPiece[5]= new Array(0, 1, 0,-1);
 		this.yPiece[5]= new Array(0, 0, 1, 1);
 		
 		//Piece --
-		this.xPiece[6]= new Array(0, 1,-1,-2);
+		this.xPiece[6]= new Array(0, 1,-2,-1);
 		this.yPiece[6]= new Array(0, 0, 0, 0);
 		
 		//Piece []
@@ -283,8 +281,8 @@ $(document).ready(function(){
 		for (var i=0;i<this.nSquares;i++) {
 			var X=this.curX+this.dx[i];
 			var Y=this.curY+this.dy[i];
-			if (0<=Y && Y<this.pForeground.rows && 0<=X && X<this.pForeground.cols) 
-				this.pForeground.matrix[Y][X].setFill(null);
+//			if (0<=Y && Y<this.pForeground.rows && 0<=X && X<this.pForeground.cols) 
+			this.pForeground.matrix[Y][X].setFill(null);
 	  	}
 	};
 	
@@ -292,8 +290,14 @@ $(document).ready(function(){
 		for (var i=0;i<this.nSquares;i++) {
 			var X=this.curX+this.dx[i];
 			var Y=this.curY+this.dy[i];
-			if (0<=Y && Y<this.pForeground.cols || 0<=X && X<this.pForeground.rows) 
+//			if (0<=Y && Y<this.pForeground.cols || 0<=X && X<this.pForeground.rows) 
+			if (this.mode == '1color')
 				this.pForeground.matrix[Y][X].setFill("red");
+			else
+				if(i%2==0)
+					this.pForeground.matrix[Y][X].setFill("red");
+				else
+					this.pForeground.matrix[Y][X].setFill("green");
 		 }
 		this.pForeground.foregroundLayer.draw();
 	};
@@ -304,8 +308,8 @@ $(document).ready(function(){
 		for (var i=0;i<this.nSquares;i++) {
 			X=this.curX+this.dx[i];
 			Y=this.curY+this.dy[i];
-			if (0<=Y && Y<this.pForeground.cols || 0<=X && X<this.pForeground.rows)
-				this.pForeground.matrix[Y][X].setFill("blue");
+//			if (0<=Y && Y<this.pForeground.cols || 0<=X && X<this.pForeground.rows)
+			this.pForeground.matrix[Y][X].setFill("blue");
 		}
 		if (Y<this.skyline)
 			this.skyline=Y;
@@ -333,23 +337,63 @@ $(document).ready(function(){
 		}
 	};
 	
-	function Game(background, foreground) {
+	Piece.prototype.eventsGame= function(){
+		$(".btn-primary").on("click", $.proxy(function(){
+            this.mode = $(event.target).find('input').val();
+	    },this));
+	};
+	
+	function Game(background, foreground, menu) {
 		this.gBackground = background;
 		this.gForeground = foreground;
 		this.gPiece = new Piece(this.gForeground);
-
+		this.gMenu = menu;
+		
+		this.pauseState = false;
 		this.level = new Array(1000,700,600,500,400,300,200,100,50,10);
 //		this.gravity = this.level[($("#fallSpeed").val()-1)];
 		this.gravity=10;
 		
-		this.startGame();
+//		this.startGame();
+		this.gMenu.mainMenu.on('mouseup touchend',$.proxy(this, "startGame"));
+		this.gMenu.pause.on('mousedown touchstart',$.proxy(this, "clickPause"));
+		this.gMenu.full.on('mousedown touchstart',$.proxy(this, "toggleFullScreen"));
+		this.gMenu.restart.on('mousedown touchstart',$.proxy(this, "restartGame"));
 	};
 	
 	Game.prototype.startGame = function(){
-		this.gPiece.getPiece();
-		this.gPiece.drawPiece();
-		this.enableKeyboard();
-		this.startAnimation();
+		this.gMenu.box.setOpacity(0.8);
+        this.gMenu.mainMenu.setScale(1);
+        setTimeout($.proxy(function(){
+        	this.gMenu.mainMenu.hide();
+        	this.gMenu.backBoxTween.play();
+        	this.gPiece.getPiece();
+    		this.gPiece.drawPiece();
+        	this.startAnimation();
+        	this.enableKeyboard();
+    	}, this), 100);
+	    
+        this.gMenu.menuLayer.draw();
+	};
+	
+	Game.prototype.clickPause= function(){
+		if (this.pauseState)
+			this.resumeGame();
+		else
+			this.pauseGame();
+		this.pauseState = !this.pauseState;
+	};
+	
+	Game.prototype.pauseGame = function(){
+		this.stopAnimation();
+		this.gMenu.clickPause();
+	};
+	
+	Game.prototype.resumeGame = function(){
+		this.gMenu.clickPause();
+		setTimeout($.proxy(function(){
+			this.startAnimation();
+		},this),1000);
 	};
 	
 	Game.prototype.startAnimation = function(){
@@ -357,13 +401,14 @@ $(document).ready(function(){
 	};
 	
 	Game.prototype.startFallPiece = function(){
-		setTimeout($.proxy(function(){
-		this.idAnim = requestAnimationFrame(this.startFallPiece.bind(this));		
+		this.idTimeout = setTimeout($.proxy(function(){
+			this.idAnim = requestAnimationFrame(this.startFallPiece.bind(this));
 			this.fall();
 		},this), 200);
 	};
 	
 	Game.prototype.stopAnimation = function(){
+		clearTimeout(this.idTimeout);
 		cancelAnimationFrame(this.idAnim);
 	};
 	
@@ -385,7 +430,40 @@ $(document).ready(function(){
 				this.stopAnimation();
 			this.gPiece.getPiece();
 		}
-		
+	};
+	
+	Game.prototype.toggleFullScreen = function(){
+		var elem=$("#container")[0];
+		if (isMobile()){
+			$("#container").width('100%');
+            $("#container").height('100%');
+			$("#container").width($(window).width());
+            $("#container").height( $(window).height());
+            
+			$("#container").css("top",-$("#container").position().top+'px');
+			//this.resizeWindow();
+		}
+		else{
+			if (!screenfull.isFullscreen){
+				this.oldWidth = $("#container").width();
+				this.oldHeight = $("#container").height();
+				$("#container").width('100%');
+	            $("#container").height('100%');
+			}
+			else{
+				$("#container").width(this.oldWidth);
+			    $("#container").height(this.oldHeight);
+			}
+			screenfull.toggle(elem);
+		}
+	};
+	
+	Game.prototype.restartGame = function(){
+		stage.clear();
+		var background = new Background();
+		var foreground = new Foreground();
+		var menu = new initMenu(stage);
+		new Game(background, foreground,menu);
 		
 	};
 	
@@ -411,7 +489,7 @@ $(document).ready(function(){
 		    }
 		},this), false);
 	};
-
+	
 	Game.prototype.KeyboardController = function(keys, repeat){
 		var timers= {};
 
